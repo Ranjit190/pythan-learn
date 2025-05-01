0ee839a84cc7b33c00a8cac5d87d43c0370c30f9
Ranjit190 <ranjits@badbeat.com>
2025-05-01 12:00:23 +0530

    Day one

--- test.py
+++ test.py
@@ -7,11 +7,24 @@
 # print("New lines can be created with a \ and the letter n")
 # input("What is your name?")
 # print("Hello "+ input("What is your name?")+"!")
-name = input("What is your name?")
-length = len(name)
-print("Hello "+name +"\nit's length is {length}".format(length=length))
-print("Hello "+name +"\nit's length is",length)
-print("Hello "+name +'\nit s length is %d' % (length))
-print("Hello "+name +'\nit s length is '+ str(length))
+# name = input("What is your name?")
+# length = len(name)
+# print("Hello "+name +"\nit's length is {length}".format(length=length))
+# print("Hello "+name +"\nit's length is",length)
+# print("Hello "+name +'\nit s length is %d' % (length))
+# print("Hello "+name +'\nit s length is '+ str(length))
 
-# print(length)
+# name = input('What is your name?')
+# length = len(name)
+# print("Your name is " + name + ' {length}'.format(length=length))
+#
+# print("Your name is " + name + ' %d' % (length))
+
+# glass1 = "milk"
+# glass2 = "juice"
+# print('you are not allowed to typrthe words "{milk}" or "{juice}"'.format(milk=glass1, juice=glass2))
+
+cityName = input("What's your city name where you grow up?\n")
+petName =  input("What's your pet name?\n")
+bandName = cityName+petName
+print('your band name is '+cityName+" "+petName)
