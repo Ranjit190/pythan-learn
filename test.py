a9132b4ae8e7ebd379302b4d58673885fb6a245b
Ranjit190 <ranjits@badbeat.com>
2025-05-25 16:33:10 +0530

    bmi calculater

--- test.py
+++ test.py
@@ -1,30 +1,13 @@
-# print("Hello world\n ranjit")
-# print("Hello"+" Angela")
-# print("Notes from Day 1")
-# print("The print statement is used to output strings")
-# print("Strings are strings of characters")
-# print("String Concatenation is done with the + sign")
-# print("New lines can be created with a \ and the letter n")
-# input("What is your name?")
-# print("Hello "+ input("What is your name?")+"!")
-# name = input("What is your name?")
-# length = len(name)
-# print("Hello "+name +"\nit's length is {length}".format(length=length))
-# print("Hello "+name +"\nit's length is",length)
-# print("Hello "+name +'\nit s length is %d' % (length))
-# print("Hello "+name +'\nit s length is '+ str(length))
+weight = 85
+height = 1.85
 
-# name = input('What is your name?')
-# length = len(name)
-# print("Your name is " + name + ' {length}'.format(length=length))
-#
-# print("Your name is " + name + ' %d' % (length))
+bmi = weight / (height ** 2)
 
-# glass1 = "milk"
-# glass2 = "juice"
-# print('you are not allowed to typrthe words "{milk}" or "{juice}"'.format(milk=glass1, juice=glass2))
-
-cityName = input("What's your city name where you grow up?\n")
-petName =  input("What's your pet name?\n")
-bandName = cityName+petName
-print('your band name is '+cityName+" "+petName)
+if bmi>=25 :
+    print("overweight")
+elif bmi>=18.5 and bmi < 25:
+    print("normal weight")
+else:
+    print("underweight")
+# 🚨 Do not modify the values above
+# Write your code below 👇
