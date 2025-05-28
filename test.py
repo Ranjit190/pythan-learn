4b13602a30605a50aa4983afab41d142f20107ce
Ranjit190 <ranjits@badbeat.com>
2025-05-28 06:15:39 +0530

    excersises

--- test.py
+++ test.py
@@ -1,13 +1,32 @@
-weight = 85
-height = 1.85
-
-bmi = weight / (height ** 2)
-
-if bmi>=25 :
-    print("overweight")
-elif bmi>=18.5 and bmi < 25:
-    print("normal weight")
+print("Welcome to python Pizza Deliveries!!")
+size = input("What your Pizza size do you want? S, M,L: ")
+st=''
+if size == 'S':
+    st ='Add pepperoni for small Pizza? Y or N: '
+elif size == 'M':
+    st = 'Add pepperoni for medium Pizza? Y or N: '
 else:
-    print("underweight")
-# 🚨 Do not modify the values above
-# Write your code below 👇
+    st = 'Add pepperoni for large Pizza? Y or N: '
+pepperoni = input(st)
+
+extra_chees = input("Do you want to extra chees? Y or N: ")
+
+calculate = 0
+if size =='S':
+    calculate= calculate+15
+elif size == 'M':
+    calculate = calculate + 20
+else:
+    calculate = calculate + 25
+
+
+if pepperoni == 'Y' and size == 'S':
+    calculate = calculate+2
+elif pepperoni == 'Y' and size == 'M':
+    calculate = calculate+3
+elif pepperoni == 'Y' and size == 'L':
+    calculate = calculate + 4
+
+if extra_chees == 'Y':
+    calculate+=1
+print('Your final bill is '+str(calculate))
