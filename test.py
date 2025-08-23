e4736104f2815746e4adc55b89354dcda889438a
Ranjit190 <ranjits@badbeat.com>
2025-08-23 12:48:08 +0530

    test

--- test.py
+++ test.py
@@ -1,24 +1,41 @@
 import random
+word_list = ["Dog", "baboon","camel"]
+random_word = random.choice(word_list)
+print(random_word+'\n')
+result = ""
+correct_letter=[]
+count = 6
+while result !=random_word:
+    latter = input("Enter letter: ")
+    st = ""
+    check = False
+    for char in random_word:
+        if char == latter:
+            st = st + char
+            correct_letter.append(char)
+            check=True
+        elif char in correct_letter:
+            st = st + char
+        else:
+            st = st + "_"
+    if check == False:
+        count -=1
+        print("You have "+str(count)+" live left.")
+        if count == 0:
+            print("You lose the Game")
+            result = random_word
+            break
+    else:
+        check = False
 
-print("Password generator:")
-uppercaseChars = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
-lowercaseChars = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-upAndLow = uppercaseChars+lowercaseChars
-numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-specialChars = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '_', '+', '=', '[', ']', '{', '}', '|', '/\\/', ':', ';', '"', "'", '<', '>', ',', '.', '?', '/', '~', '`']
+    print(st)
+    result =st
 
-enterCh = int(input("Enter number of character in your password:\n"))
-enterNum = int(input("Enter number of number in your password:\n"))
-EnterSpecialCh = int(input("Enter number of special character in  your password:\n"))
-passwordList = []
-for ch in range(0, enterCh+1):
-    passwordList.append(random.choice(upAndLow))
-for ch in range(0, enterNum+1):
-    passwordList.append(str(random.choice(numbers)))
-for ch in range(0, EnterSpecialCh+1):
-    passwordList.append(random.choice(specialChars))
-random.shuffle(passwordList)
-password = ''
-for st in passwordList:
-    password = password+st
-print(password)
+if count !=0:
+    print("Final: "+result)
+
+
+
+
+
+
